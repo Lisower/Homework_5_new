dd87fcf641eb8cfe07266f31ea1f2febba34a039
Lisower <144545336+Lisower@users.noreply.github.com>
2024-04-30 19:59:06 +0300

    Update login.php

--- login.php
+++ login.php
@@ -9,10 +9,6 @@ $session_started = false;
 if ($_COOKIE[session_name()] && session_start()) {
   $session_started = true;
   if (!empty($_SESSION['login'])) {
-    // Если есть логин в сессии, то пользователь уже авторизован.
-    // TODO: Сделать выход (окончание сессии вызовом session_destroy()
-    //при нажатии на кнопку Выход).
-    // Делаем перенаправление на форму.
     header('Location: ./');
     exit();
   }
