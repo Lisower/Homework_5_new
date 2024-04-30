e6e21d74d77e7b107f9f474bda626f0b35a120fd
Lisower <144545336+Lisower@users.noreply.github.com>
2024-04-30 15:07:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const Button = document.getElementById('Button');
+const Exit = document.getElementById('Exit');
 const Popup = document.getElementById('Popup');
 const Form = document.getElementById('Form');
 
 Button.addEventListener('click', () => {
     Popup.style.display = 'block';
+});
+
+Exit.addEventListener('click', () => {
+    Popup.style.display = 'none';
 });
 
 window.addEventListener('popstate', () => {
