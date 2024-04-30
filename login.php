5fa55aaab167ddaced5510a76a282ea8ffac3c94
Lisower <144545336+Lisower@users.noreply.github.com>
2024-04-30 18:37:32 +0300

    Update login.php

--- login.php
+++ login.php
@@ -44,7 +44,7 @@ else {
   include('credentials.php');
   $db = new PDO('mysql:host=localhost;dbname=u67447', $GLOBALS['user'], $GLOBALS['pass'],
     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-  $stmt = $db->prepare("SELECT id FROM table Applications WHERE login = ? AND pass = ?");
+  $stmt = $db->prepare("select id from Applications where login = ? and pass = ?");
   $stmt->execute([$_POST['login'],$_POST['pass']]);
   $stmt->store_result();
   $row_count = $stmt->num_rows;
