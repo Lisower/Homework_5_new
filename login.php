a7355edce8ff2ba0183123b6982d05df6a054046
Lisower <144545336+Lisower@users.noreply.github.com>
2024-04-30 19:26:59 +0300

    Update login.php

--- login.php
+++ login.php
@@ -39,6 +39,7 @@ else {
   $row_count = $stmt->rowCount();
   if ($row_count <= 0) {
       print('Пользователя с такими логином и паролем нет в базе данных!');
+      print(md5(41cc4c7))
       // header('Location: login.php');
       exit();
   }
